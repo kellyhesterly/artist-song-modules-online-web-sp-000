7c7fe9f8a0cf02d4e9aa5a250095ca4e12726633
Kelly Hesterly <kelly.hesterly@gmail.com>
2020-10-27 19:15:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,14 +5,11 @@ class Song
   attr_reader :artist
 
   extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
 
   @@songs = []
-
-  def initialize
-    self.class.all << self
-  end
 
   def self.all
     @@songs
